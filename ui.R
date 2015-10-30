ffd8c2f62d81e40c0b0383a6dc029df512742715
Gregoire Vincke <gregoire.vincke@ataya.be>
2015-10-30 22:32:45 +0100

    Set language select in a tga placed in navbarPage() header, and placed over navbarPage() with CSS

--- ui.R
+++ ui.R
@@ -17,10 +17,10 @@ Sys.setlocale("LC_ALL", "fr_FR.UTF-8")#to be sure that accents in text will be a
 library(shiny)
 
 # Define UI for zplot
-shinyUI(navbarPage("Belgium Pigeon Racing Map",
-  tabPanel(uiOutput('uiPanelTitle'),
+shinyUI(navbarPage("Belgium Pigeon Racing Map",id="main",#http://shiny.rstudio.com/reference/shiny/latest/navbarPage.html
+                   header=tags$div(id="lang-div",selectInput("language",uiOutput('uiSBlanguage'),list("English" = "en","Français" = "fr", "Nederlands" ="nl"),selected = "fr",selectize=FALSE)),#Must be defined in UI.R instead of server.R to avoid error of type "trying to select more than one value because input$language is only set after server.R load                
+  tabPanel(uiOutput('uiPanelTitle'),value="main",
   fluidPage(
-  
   tags$head(
     tags$head(includeScript("www/js/google-analytics.js")),
     tags$script(type="text/javascript",src="js/scripts.js"),
@@ -30,7 +30,7 @@ shinyUI(navbarPage("Belgium Pigeon Racing Map",
     #tags$style(type="text/css", '.checkbox input[type="checkbox"],.radio input[type="radio"] { float: none; }'),
     tags$style(type="text/css", 'input[type="text"] { float: none; display : inline;}'),
     tags$style(type="text/css", '.checkbox,.radio { float: none; display:inline; margin-top:5px; margin-bottom:5px;line-height:15px;}'),
-    tags$style(type='text/css', "select#selection,select#mapzones,select#racedist,select#language { width: 200px; display : inline; }"),
+    tags$style(type='text/css', "select#selection,select#mapzones,select#racedist { width: 200px; display : inline; }"),
     tags$style(type='text/css', "select#distunit { width: 65px; display : inline; }"),
     tags$style(type='text/css', "input#round { display : inline; }"),
     tags$style(type="text/css", 'input[type="number"] { width: 50px; }'),
@@ -40,12 +40,13 @@ shinyUI(navbarPage("Belgium Pigeon Racing Map",
     tags$style(type='text/css', "#Lon, #Lat { max-width: 100px; }"),
     tags$style(type='text/css', "#note { color:#ff0000;font-size:xx-small; }"),
     tags$style(type="text/css", "h1,h2,h3,h4,h5,h6 {color:#317EAC;}"),
-    tags$style(type="text/css", "hr {margin-bottom:8px; margin-top:8px;}")
+    tags$style(type="text/css", "hr {margin-bottom:8px; margin-top:8px;}"),
+    tags$style(type='text/css', "select#language { width: 150px; display : inline; }"),
+    tags$style(type="text/css", "#lang-div {z-index:1000;position:absolute;top:5px;right:130px;}")
   ),
   fluidRow(
     column(3,
       wellPanel(
-        selectInput("language",uiOutput('uiSBlanguage'),list("English" = "en","Français" = "fr", "Nederlands" ="nl"),selected = "fr",selectize=FALSE),#Must be defined in UI.R instead of server.R to avoid error of type "trying to select more than one value because input$language is only set after server.R load
         uiOutput('uiSBtop'),
         selectInput(inputId="towns",label=uiOutput("uiSBTowns"),choices="",selectize=FALSE,multiple=TRUE),#Label is translated, so have to be set in server.R, but the list must be set in UI.R to be setted before of server.R computation to be filled by Towns values : soit un select est défini ici avec choices ="" et un observe dans server.R le rempli par après; soit le select est directement défini dans server.R mais du coup ne peut être exploité par un script js
         selectInput(inputId="selection",label=uiOutput("uiSBSelection"),choices="",selected="itawc",selectize=FALSE),#selection must be define in UI.R to be reactive to script.js -> create emty select here, use observe to load it with translated content, and uiOutput to have translated label
@@ -67,7 +68,7 @@ shinyUI(navbarPage("Belgium Pigeon Racing Map",
   )
 )
 ),
-tabPanel(uiOutput('uiCreditsTitle'),
+tabPanel(uiOutput('uiCreditsTitle'),value="credits",
          fluidPage(
            tags$head(
              tags$style(type='text/css', ".col-sm-8 { width: 100%; }")
